ec50c3414d38f938e8a5f5dea1e04d5584efa5a7
Vinit <vneogi199@gmail.com>
2018-02-20 00:01:32 -0800

    added code for connectivity

--- Extension/app.js
+++ Extension/app.js
@@ -1,22 +1,5 @@
 var whois = require('whois-json');
 
-// whois('google.com', function(err, result){
-// 	console.log(JSON.stringify(result, null, 2))
-// })
-function registrationLength(){
-	whois('google.com', function(err, result){
-		if(JSON.stringify(result.registryExpiryDate, null, 2).replace('"','').split('-')[0]-JSON.stringify(result.creationDate, null, 2).replace('"','').split('-')[0]<1){
-			console.log(1);
-			//return 1;
-		}
-		else if(JSON.stringify(result.registryExpiryDate, null, 2).replace('"','').split('-')[0]-JSON.stringify(result.creationDate, null, 2).replace('"','').split('-')[0]==1){
-			if(JSON.stringify(result.creationDate, null, 2).replace('"','').split('-')[1]+1-JSON.stringify(result.creationDate, null, 2).replace('"','').split('-')[1]>1)
-			console.log(-1);
-			// return -1;
-			else console.log(1);
-			// else return 1;
-		}
-		else console.log(1);
-		// else return 1;
-	}).then(console.log(registrationLength()););
-}
+whois('google.com', function(err, result){
+	console.log(JSON.stringify(result, null, 2))
+})
