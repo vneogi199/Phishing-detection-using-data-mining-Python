a7b62aab8a48b7eb4b8708175363f79c2874ac41
Vinit Neogi <vneogi199@gmail.com>
2018-03-06 16:31:07 +0530

    Code for fetching HTML/JS features

--- Extension/rough.js
+++ Extension/rough.js
@@ -32,7 +32,8 @@ var input = `@relation phishing
 @attribute Result {-1,1}
 
 @data
-IP1,Length2,Short3,At4,DSlash5,PreSuf6,SbDom7,8,9,Fav10,11,HTTPSToken12,Req13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,-1    
+IP1,Length2,Short3,At4,DSlash5,PreSuf6,SbDom7,8,9,Fav10,11,HTTPSToken12,Req13,
+Anchor14,Links15,16,Mail17,18,19,Status20,RClick21,Popup22,Iframe23,24,25,26,27,28,Stat29,-1    
 `;
 
 //1 having_IP_Address
@@ -64,7 +65,10 @@ input=window.location.hostname.match(/-/i)?input.replace("PreSuf6","1"):input.re
 
 //7 having_Sub_Domain
 var TLDs = ["ac", "ad", "ae", "aero", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "arpa", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "museum", "mv", "mw", "mx", "my", "mz", "na", "name", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "travel", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "xn--0zwm56d", "xn--11b5bs3a9aj6g", "xn--3e0b707e", "xn--45brj9c", "xn--80akhbyknj4f", "xn--90a3ac", "xn--9t4b11yi5a", "xn--clchc0ea0b2g2a9gcd", "xn--deba0ad", "xn--fiqs8s", "xn--fiqz9s", "xn--fpcrj9c3d", "xn--fzc2c9e2c", "xn--g6w251d", "xn--gecrj9c", "xn--h2brj9c", "xn--hgbk6aj7f53bba", "xn--hlcj6aya9esc7a", "xn--j6w193g", "xn--jxalpdlp", "xn--kgbechtv", "xn--kprw13d", "xn--kpry57d", "xn--lgbbat1ad8j", "xn--mgbaam7a8h", "xn--mgbayh7gpa", "xn--mgbbh1a71e", "xn--mgbc0a9azcg", "xn--mgberp4a5d4ar", "xn--o3cw4h", "xn--ogbpf8fl", "xn--p1ai", "xn--pgbs0dh", "xn--s9brj9c", "xn--wgbh1c", "xn--wgbl6a", "xn--xkc2al3hye2a", "xn--xkc2dl3a5ee0h", "xn--yfro4i67o", "xn--ygbi2ammx", "xn--zckzah", "xxx", "ye", "yt", "za", "zm", "zw"];
+
 var domain_list=["esy.es","hol.es" ,"000webhostapp.com" ,"16mb.com" ,"bit.ly","for-our.info", "beget.tech" ,"blogspot.com","weebly.com","raymannag.ch" ];
+var ips_list=["146.112.61.108","31.170.160.61","67.199.248.11","67.199.248.10","69.50.209.78","192.254.172.78","216.58.193.65","23.234.229.68","173.212.223.160","60.249.179.122"];
+
 function getDomain(url,value){
     var parts = url.split('.');
     if (parts[0] === 'www'){
@@ -82,14 +86,29 @@ function getDomain(url,value){
         }
     if(value==2)
         {
+        	if (window.location.hostname.match(/[a-z]/i))
+        	{
+
             for(domain in domain_list){             
                 if(domain_list[domain]==parts[parts.length-2].concat(".",parts[parts.length-1]))
                     return "1";                
                 } 
-            return "-1";
+            return "-1";}
+
+            else
+            {
+            	for(ip in ips_list){             
+                	if(ips_list[ip]==window.location.hostname)
+                    	return "1";                
+            	} 
+            	return "-1";
+            }
         }                   
 }
+
 input=input.replace("SbDom7",getDomain(window.location.hostname,1));
+
+
 
 //10 favicon
 function getFavicon()
@@ -101,10 +120,17 @@ function getFavicon()
     	if((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon"))
         {
         	favicon = nodeList[i].getAttribute("href");
-        }}
-    if(favicon.indexOf("http") >=0 || favicon.indexOf("https") >=0)
+        }
+    }
+    if(favicon){
+    	if(favicon.indexOf("http") >=0 || favicon.indexOf("https") >=0)
 			return "1"; 
-	else return "-1";
+		else return "-1";
+    }
+    else{
+    	return "1";
+    }
+    
 }
 input=input.replace("Fav10",getFavicon());
 
@@ -113,6 +139,17 @@ input=input.replace("Fav10",getFavicon());
 input=(window.location.hostname.search('https')!=-1)?input.replace("HTTPSToken12","1"):input.replace("HTTPSToken12","-1");
 
 //13 requesturl
+function getSource(word)
+{
+    var nodeList = document.getElementsByTagName(word);
+    var Code=[];
+     for (var i = 0; i < nodeList.length; i++)
+     {
+          Code[i] = nodeList[i].getAttribute("src");
+     }
+     Code = Code.filter(function(n){ return n != undefined }); 
+    return Code;
+}
 function getrequesturl()
 {
 	p=0;
@@ -139,33 +176,7 @@ function getrequesturl()
 }
 input=input.replace("Req13",getrequesturl());
 
-
-
-
-console.log(input);
-
-//var ips_list=["146.112.61.108","31.170.160.61","67.199.248.11","67.199.248.10","69.50.209.78","192.254.172.78","216.58.193.65","23.234.229.68","173.212.223.160","60.249.179.122"];
-
-function findWord(word) {
-	Code=document.getElementsByTagName('html')[0].innerHTML;
-	if (Code.indexOf(word) >=0) {
-		return 1;
-	}
-    else 
- 	return -1; 
-}
-
-function getSource(word)
-{
-    var nodeList = document.getElementsByTagName(word);
-    var Code=[];
-     for (var i = 0; i < nodeList.length; i++)
-     {
-          Code[i] = nodeList[i].getAttribute("src");
-     }
-     Code = Code.filter(function(n){ return n != undefined }); 
-    return Code;
-} 
+//14 URL_of_Anchor
 function getURLs(word)
 {
 	var nodeList = document.getElementsByTagName(word);
@@ -179,16 +190,13 @@ function getURLs(word)
     return Code;
            
 } 
-
 function getanchorurl()
 {
 	p=0;
 	Anchor_URL=getURLs("a");
-
 	c=Anchor_URL.length;
 	for (a in Anchor_URL)
 	{ 
-		
 		if(Anchor_URL[a].indexOf("#") >=0  || Anchor_URL[a].indexOf(":") >=0)
 			p=p+1;
 		else
@@ -197,12 +205,11 @@ function getanchorurl()
 				p=p+1;
 		}
 	}
-
 	return p/c<0.31?-1:p/c<0.67?0:1;
 }
- 
+input=input.replace("Anchor14",getanchorurl()); 
 
-
+//15 Links_in_tags
 function gettagurls()
 {		
 	p=0;
@@ -238,15 +245,79 @@ function gettagurls()
 	}
 	return p/c<0.17?-1:p/c<0.81?0:1;	
 }
+input=input.replace("Links15",gettagurls());
+
+//16 SFH
+function findHttp()
+{
+ 	
+	var nodeList = document.getElementsByTagName("form");
+	var Code=[];
+    for (var i = 0; i < nodeList.length; i++)
+     {
+          Code[i] = nodeList[i].getAttribute("action");
+     }
+    if(Code){
+    	for (a in Code)
+		{ 	
+			if (Code[a]=="" || Code[a]=="about:blank")
+				return "1";
+			else if(Code[a].indexOf("http") >=0 || Code[a].indexOf("https") >=0)
+				return "0"; 
+		}
+		return "-1";
+    	
+    }
+    else
+    	return "-1";    
+}
+
+input=input.replace("SFH16",findHttp());
+
+//17 Submitting_to_email
+function findWord(word) {
+	Code=document.getElementsByTagName('html')[0].innerHTML;
+	if (Code.indexOf(word) >=0) {
+		return 1;
+	}
+    else 
+ 		return -1; 
+}
+
+input=input.replace("Mail17",findWord('action="mailto'));
 
 
-//10
-iframe=findWord("iframe");
-//11
-stat_report=getDomain(window.location.hostname,2);
-//12
-anchorurl=getanchorurl(); 
+//20 on_mouseover
 
-//14
-tagurls=gettagurls();
+input=input.replace("Status20",findWord('onmouseover="window.status'));
+
+//21 RightClick
+input=input.replace("RClick21",findWord("event.button==2"));
+
+//22 popUpWidnow
+function findPopupForm(word) {
+	if (window.opener)	{
+		Code=document.getElementsByTagName('html')[0].innerHTML;
+		if (Code.indexOf(word) >=0) 
+			return 1;
+    	else
+    		return -1;
+	}
+ 	else
+ 		return -1;
+}
+input=input.replace("Popup22",findWord("form"));
+
+//23 iframe
+input=input.replace("Iframe23",findWord("iframe"));
+
+//29 Statistical_report
+
+input=input.replace("Stat29",getDomain(window.location.hostname,2));
+
+
+
+console.log(input);
+
+
 
